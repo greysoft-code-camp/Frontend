d8b8df88e8e59b578d293b3db863db534a9cdc30
Slenzy2 <tobiikupolati33@gmail.com>
2022-03-07 10:07:34 +0100

    tobi

---
+++ src/components/Board.vue
@@ -0,0 +1,31 @@
+<template>
+   <q-card
+      class="text-white col-4 q-mx-sm q-mt-sm"
+      style="background: #014a88; height:100px; width:250px"
+      clickable v-ripple
+      to=""
+    >
+      <q-card-section class="flex items-center justify-between">
+        <div class="text-subtitle1">Lorem</div>
+         <q-checkbox
+          v-model="val"
+          color="white"
+          checked-icon="star"
+          unchecked-icon="star_border"
+          indeterminate-icon="help"
+        />
+      </q-card-section>
+    </q-card>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    return {
+      val: ref(false)
+    }
+  }
+}
+</script>
---
+++ src/components/Boards.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row q-pl-xl q-pt-lg">
+    <div v-for="n in 5" :key="n" class=" q-mx-sm  cursor-pointer"  style="height: 120px">
+      <Board />
+    </div>
+
+  </div>
+
+
+
+
+</template>
+
+<script>
+import Board from 'components/Board.vue'
+
+export default {
+  name: 'Boards',
+  components:{
+    Board
+  }
+}
+</script>
+
+<style >
+  .my-content{
+  padding: 10px 15px;
+  background: rgba(86,61,124,.15);
+  border: 1px solid rgba(86,61,124,.2);
+  }
+</style>
